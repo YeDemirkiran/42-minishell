615c63836e7f20b1609ddbdfd7f09526a8c7789d
YeDemirkiran <yedemirkiran27@gmail.com>
2025-09-07 22:56:55 +0300

    Add macro colors

--- include/structs/e_metachars.h
+++ include/structs/e_metachars.h
@@ -6,7 +6,7 @@
 /*   By: yademirk <yademirk@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 20:19:25 by yademirk          #+#    #+#             */
-/*   Updated: 2025/09/07 21:04:38 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/09/07 22:56:01 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -20,31 +20,22 @@ enum e_metachars
 {
 	/** @brief Space character, separates tokens. */
 	METACHAR_SPACE = ' ',
-
 	/** @brief Tab character, separates tokens. */
 	METACHAR_TAB = '\t',
-
 	/** @brief Newline, separates commands or lines. */
 	METACHAR_NEWLINE = '\n',
-
 	/** @brief Pipe operator, redirects output to another command. */
 	METACHAR_PIPE = '|',
-
 	/** @brief Ampersand, for background processes or logical AND. */
 	METACHAR_AMPERSAND = '&',
-
 	/** @brief Semicolon, separates commands on the same line. */
 	METACHAR_SEMICOLON = ';',
-
 	/** @brief Left parenthesis, begins subshell or grouping. */
 	METACHAR_PAREN_LEFT = '(',
-
 	/** @brief Right parenthesis, ends subshell or grouping. */
 	METACHAR_PAREN_RIGHT = ')',
-
 	/** @brief Less-than, input redirection operator. */
 	METACHAR_REDIR_LEFT = '<',
-
 	/** @brief Greater-than, output redirection operator. */
 	METACHAR_REDIR_RIGHT = '>'
 };
--- src/main/main.c
+++ src/main/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yademirk <yademirk@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/07 11:06:42 by yademirk          #+#    #+#             */
-/*   Updated: 2025/09/07 22:46:03 by yademirk         ###   ########.fr       */
+/*   Created: 2025/09/07 22:56:18 by yademirk          #+#    #+#             */
+/*   Updated: 2025/09/07 22:56:18 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -16,12 +16,13 @@
 #include <readline/readline.h>
 
 #include <modules/builtin.h>
+#include <macros/m_colors.h>
+
+#define SHELL_NAME "babashell"
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*str;
-	// char	*user_name;
-	// char	*cwd;
 	char	first_run;
 
 	(void)argc;
@@ -31,15 +32,9 @@ int	main(int argc, char **argv, char **envp)
 	while (str || first_run)
 	{
 		if (!first_run)
-		{
 			free(str);
-			// printf("\n");
-		}
 		first_run = 0;
-		// user_name = getenv("USER");
-		// cwd = getcwd(NULL, 0);
-		//printf("\n(minishell) %s:%s$", user_name, cwd);
-		str = readline("minishell > ");
+		str = readline(COLOR_GREEN SHELL_NAME COLOR_WHITE " > ");
 		printf("%s\n", str);
 	}
 	return (EXIT_SUCCESS);
